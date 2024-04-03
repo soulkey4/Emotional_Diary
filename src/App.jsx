bc8d2866a8cabc7f1e829e763d0f33036b1ef642
DONGUK <kkdw428@naver.com>
2024-04-04 00:31:49 +0900

    f

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import New from './pages/New'
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
-
+//
 // <Routes> 안에는 <Route> 만들어갈 수 있음!
 function App() {
   return (
